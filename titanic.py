761a58a803c0c8a432724e489f26009cc77de096
Sarvesh Relekar <sar265278@gmail.com>
2019-05-11 23:44:10 +0530

    Solution Rank : 4079

--- titanic.py
+++ titanic.py
@@ -1,72 +1,102 @@
 import pandas as pd
 import numpy as np
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import Imputer
-from sklearn.neighbors import KNeighborsClassifier
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score
-from sklearn.svm import SVC
-from sklearn.svm import LinearSVC
-data = pd.read_csv('/home/sarvesh/Titanic/train.csv')
-pd.set_option('display.max_columns', None)
-train = data
-train.dropna(inplace = True)
-train_target = train['Survived']
-train.drop(['PassengerId', 'Name', 'Cabin', 'Survived', 'Embarked', 'Ticket'], axis = 1, inplace = True)
-gender = {"male" : 0, "female" : 1}
-train['Sex'].replace(gender, inplace = True)
-train_columns = train
-labels = list(train.columns.values)
-labels.remove('Sex')
-print(labels)
-#for label in labels:
-    #train_columns[label].fillna(value = train_columns[label].mean(), inplace = True)
-print('Training Data :')
-print(train_columns)
-print(train_columns.describe())
-test = pd.read_csv('/home/sarvesh/Titanic/test.csv')
-passengers = test['PassengerId']
-test.drop(['PassengerId', 'Name', 'Cabin', 'Embarked', 'Ticket'], axis = 1, inplace = True)
-test['Sex'].replace(gender, inplace = True)
-test_columns = test
-for label in labels:
-    test_columns[label].fillna(value = test_columns[label].mean(), inplace = True)
-print('Testing Data :')
-print(test_columns)
-print(test_columns.describe())
-logreg = LogisticRegression(max_iter=8000)
-svm = SVC()
-param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'penalty' : ['l1', 'l2'], 'solver' : ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']}
-#'solver' : ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
-#'loss' : ['hinge', 'squared_hinge']
-#'penalty' : ['l1', 'l2']
-#'kernel' : ['linear', 'rbf', 'poly', 'rbf', 'sigmoid', 'precomputed']
-#svm.fit(train_columns, train_target)
-#svm_clf = GridSearchCV(svm, param_grid)
-#test_clf = GridSearchCV(logreg, param_grid)
-#svm_clf.fit(train_columns, train_target)
-clf = GridSearchCV(logreg, param_grid)
-clf.fit(train_columns, train_target)
-#svm_clf.fit(train_columns, train_target)
-print(clf.best_params_)
-#print(svm_clf.best_params_)
-#clf.fit(train_columns, train_target)
-#axis = 0 for rows
-#axis = 1 for columns
-#imp = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-#steps = [('imputer', imp), ('gridsearchcv', clf)]
-#pipeline = Pipeline(steps)
-#pipeline.fit(train_columns, train_target)
-#predictions = pipeline.predict(test)
-#print("LinearSVC :")
-#predictions = svm_clf.predict(test_columns) 
-#print(predictions)
-print("Logistic Regression :")
-predictions = clf.predict(test_columns)
-print(predictions)
-#print(pipeline.named_steps['gridsearchcv'].score(train, train_target))
-df = pd.DataFrame(passengers)
-df['Survived'] = predictions
-df.to_csv('/home/sarvesh/Titanic/solution.csv', index = False)
+from xgboost import XGBClassifier
+pd.set_option('display.max_columns', 15)
+
+df_train = pd.read_csv('train.csv')
+df_test = pd.read_csv('test.csv')
+
+print(df_train.head())
+print(df_train[df_train['Ticket'] == '3460'])
+
+df_train.drop(['PassengerId', 'Ticket', 'Name'], axis = 1, inplace = True)
+
+
+df_train = pd.get_dummies(df_train, columns = ['Sex'])
+df_train.drop(['Sex_male'], axis = 1, inplace = True)
+
+cabin = df_train['Cabin']
+cabin.fillna('No', inplace = True)
+cabin = cabin.values.tolist()
+new_cabin = np.array([0 if x == 'No' else 1 for x in cabin]).T
+df_train['Cabin'] = new_cabin
+
+print(df_train[df_train['Embarked'].isna()])
+df_train['Embarked'].fillna('S', inplace = True)
+df_train['Embarked'].replace({'S' : 0, 'C' : 1, 'Q' : 2}, inplace = True)
+#df_train = pd.get_dummies(df_train, columns = ['Embarked'])
+
+df_train['Age'].fillna(value = np.random.randint(low = 16, high = 37), inplace = True)
+
+
+mms = MinMaxScaler()
+age = df_train['Age'].values.reshape(-1, 1)
+age = mms.fit_transform(age)
+df_train['Age'] = age
+
+fare = df_train['Fare'].values.reshape(-1, 1)
+fare = mms.fit_transform(fare)
+df_train['Fare'] = fare
+
+print(df_train.head())
+
+y = df_train['Survived']
+X = df_train.drop(['Survived'], axis = 1)
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size = 0.25, random_state = 42)
+
+xgb = XGBClassifier()
+xgb.fit(X_train, y_train)
+y_pred = xgb.predict(X_val)
+
+print("Accuracy for Gradient Boosted Decision Trees : {}".format(accuracy_score(y_val, y_pred)))
+
+passengers = df_test['PassengerId'].values
+print(df_test[df_test['Ticket'] == '3702'])
+print(df_test.iloc[152, :])
+df_test.drop(['PassengerId', 'Ticket', 'Name'], axis = 1, inplace = True)
+
+for column in df_test.columns.tolist():
+    print("{} : {}".format(column, df_test[column].isna().sum()))
+
+print(df_test[df_test['Fare'].isna()])
+
+df_test = pd.get_dummies(df_test, columns = ['Sex'])
+df_test.drop(['Sex_male'], axis = 1, inplace = True)
+
+
+cabin = df_test['Cabin']
+cabin.fillna('No', inplace = True)
+cabin = cabin.values.tolist()
+new_cabin = np.array([0 if x == 'No' else 1 for x in cabin]).T
+df_test['Cabin'] = new_cabin
+
+#df_test = pd.get_dummies(df_test, columns = ['Embarked'])
+df_test['Embarked'].replace({'S' : 0, 'C' : 1, 'Q' : 2}, inplace = True)
+
+df_test['Age'].fillna(value = np.random.randint(low = 16, high = 37), inplace = True)
+df_test['Fare'].fillna(value = np.mean(df_test['Fare']), inplace = True)
+
+mms = MinMaxScaler()
+age = df_test['Age'].values.reshape(-1, 1)
+age = mms.fit_transform(age)
+df_test['Age'] = age
+
+fare = df_test['Fare'].values.reshape(-1, 1)
+fare = mms.fit_transform(fare)
+df_test['Fare'] = fare
+
+print(df_test.head())
+
+y_pred = xgb.predict(df_test)
+
+df = pd.DataFrame()
+df['PassengerId'] = passengers.T
+df['Survived'] = y_pred.T
+df.to_csv('solution.csv', index = False)
